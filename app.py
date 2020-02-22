85c7154d208f420b094ce60e915920dd29816300
dedenbangkit <mail@dedenbangkit.com>
2020-02-23 01:42:27 +0700

    modify string to integer when checking offline data

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import pandas as pd
 import sys
 
 df = pd.read_csv('data.csv')
-results = df[df['NIK'] == str(sys.argv[1])].to_dict('records')[0]
+results = df[df['NIK'] == int(sys.argv[1])].to_dict('records')[0]
 
 for k in results:
     if k != 'view':
         if sys.argv[2] == "blank":
-            print("{}".format(results[k].upper()))
+            print("{}".format(str(results[k]).upper()))
         else:
-            print("{}: {}".format(k.upper(),results[k].upper()))
+            print("{}: {}".format(str(k).upper(),str(results[k]).upper()))
